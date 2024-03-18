b12fe5c1f9c0990271a22e8cda9937ce48adb339
abfiker <new94abemelek@gmail.com>
2024-03-18 19:12:09 +0300

    "working on promises"

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const superagent = require('superagent');
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) return reject("I couldn't find that file 😢")
+            resolve(data)
+        })
+    })
+}
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if (err) return reject("couldn't write file 😢")
+            resolve("Success")
+        })
+    })
+}
+readFilePro(`${__dirname}/dog.txt`)
+.then(data => {
+
+    const breed = data.toString().trim(); // Trim any extra whitespace
+    console.log(`Breed: ${breed}`);
+    return superagent
+        .get(`https://dog.ceo/api/breed/${breed}/images/random`)
+
+}).then(res => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message)
+
+}).then(()=>{
+    console.log("Random dog image saved to file")
+}).catch(err => {
+    console.error('Error fetching image:', err);
+});
+
