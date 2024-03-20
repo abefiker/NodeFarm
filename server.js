57980cb05b93bee428e7545846eaf06b21d02d0f
abfiker <new94abemelek@gmail.com>
2024-03-20 10:33:33 +0300

    "connecting db with express app"

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = require('./app');
-
 dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE
+mongoose
+  // .connect(process.env.DATABASE_LOCAL ,{ 
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('Connected to MongoDB successfully!')
+  }).catch(err => {
+    console.log(err)
+  });
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`App listening on ${port}...`);
