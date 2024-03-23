09b160c8181cfe6b82763daf11aa61cfa28f8ed8
abfiker <new94abemelek@gmail.com>
2024-03-23 10:38:30 +0300

    "query middleware"

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,10 @@ const tourSchema = new mongoose.Schema({
     unique: true
   },
   slug: String,
+  secretTour: {
+    type: String,
+    default: false
+  },
   duration: {
     type: Number,
     required: [true, 'A Tour must have a duration ��']
@@ -73,6 +77,18 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc)
 //   next()
 // })
+
+//QUERY MIDDLEWARE 
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } })
+  this.start= Date.now()
+  next()
+})
+tourSchema.post('find', function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start}  milliseconds`)
+    console.log(docs)
+    next()
+})
 
 const Tour = mongoose.model('Tour', tourSchema)
 
