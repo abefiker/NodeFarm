091ff9a54a8217b05e791982e2a0926c2712795b
abfiker <new94abemelek@gmail.com>
2024-03-23 11:52:52 +0300

    "custom Data validation"

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
+const validator = require('validator')
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A Tour must have a name 😢'],
-    unique: true
+    unique: true,
+    maxlength: [40, 'a Tour name must have less or equal than 40 characters'],
+    minlenght: [10, 'a Tour name must have greater or equal than 10 characters']
+    // validate: [validator.isAlpha, 'Tour name must only have alpha characters']
   },
   slug: String,
   secretTour: {
@@ -21,11 +25,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A Tour must have a difficulty ��']
+    required: [true, 'A Tour must have a difficulty ��'],
+    enum: {
+      values: ['easy', 'meduim', 'difficult'],
+      message: 'Difficulty is either : easy , medium , difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0']
   },
   ratingQuantity: {
     type: Number,
@@ -35,7 +45,16 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A Tour must have a price 😢']
   },
-  priceDisplay: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function (value) {
+        // 'this' only points to docs on NEW documente creation
+        return value < this.price;
+      },
+      message: 'Discount price ({VALUE}) must be less than  regular price'
+    },
+  },
   summary: {
     type: String,
     required: [true, 'A Tour must have a summary ��']
@@ -81,16 +100,16 @@ tourSchema.pre('save', function (next) {
 //QUERY MIDDLEWARE 
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } })
-  this.start= Date.now()
+  this.start = Date.now()
   next()
 })
 tourSchema.post(/^find/, function (docs, next) {
-    console.log(`Query took ${Date.now() - this.start}  milliseconds`)
-    next()
+  console.log(`Query took ${Date.now() - this.start}  milliseconds`)
+  next()
 })
-tourSchema.pre('aggregate',function(next){
-    console.log(this.pipeline())
-    next()
+tourSchema.pre('aggregate', function (next) {
+  console.log(this.pipeline())
+  next()
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
