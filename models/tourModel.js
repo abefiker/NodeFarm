1a24ebfc74fe6434f08d5e05b709170ea29a873c
abfiker <new94abemelek@gmail.com>
2024-03-23 11:09:47 +0300

    "aggregate middleware"

--- models/tourModel.js
+++ models/tourModel.js
@@ -84,9 +84,12 @@ tourSchema.pre(/^find/, function (next) {
   this.start= Date.now()
   next()
 })
-tourSchema.post('find', function (docs, next) {
+tourSchema.post(/^find/, function (docs, next) {
     console.log(`Query took ${Date.now() - this.start}  milliseconds`)
-    console.log(docs)
+    next()
+})
+tourSchema.pre('aggregate',function(next){
+    console.log(this.pipeline())
     next()
 })
 
