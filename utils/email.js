22e2def9573dc408922045a8e6be7285eb8a4a21
abfiker <new94abemelek@gmail.com>
2024-03-28 09:26:08 +0300

    send email

---
+++ utils/email.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer')
+const sendEmail = async options => {
+    //1) create transport
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    })
+    //2) Define the email options
+    const mailOptions = {
+        from: process.env.EMAIL_FROM,
+        to: options.email,
+        subject: options.subject,
+        text: options.text
+    }
+    //3) Send the email
+    await transport.sendMail(mailOptions)
+}
+
+module.exports = sendEmail 
