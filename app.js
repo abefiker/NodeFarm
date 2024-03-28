2e51b144ef80b57550a70a3396344efc68e53a8a
abfiker <new94abemelek@gmail.com>
2024-03-29 00:07:52 +0300

    setting security http header

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const app = express()
-const { rateLimit } = require('express-rate-limit')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 
 const morgan = require('morgan')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
-//1) Middlewares
-app.use(express.json())
+//1) Global Middlewares
+//set secure http headers
+app.use(helmet())
+//development logging 
 if (process.env.NODE_ENV !== 'development') {
     app.use(morgan('dev'))
 }
-
+//body parser , reading data from body to req.body 
+app.use(express.json({ limit: '10kb' }))
+//serving static files
 app.use(express.static(`${__dirname}/public`))
+//Test middleware
 app.use((req, res, next) => {
     console.log('Hello from the middleware 👋')
     next()
 })
+//limiting request from the same API
 const limiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
