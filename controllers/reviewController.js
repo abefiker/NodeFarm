5aba3cb2b706b159f41e6d4f18483b6cb87495b9
abfiker <new94abemelek@gmail.com>
2024-04-01 16:39:23 +0300

    implementing simple nested route

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,9 @@
 const Review = require('../models/reviewModel')
 const catchAsync = require('../utils/catchAsync')
 exports.createReview = catchAsync(async (req, res, next) => {
+    //Allow nested routes
+    if (!req.body.tour) req.body.tour = req.params.tourId
+    if (!req.body.user) req.body.user = req.user.id
     const newReview = await Review.create(req.body)
     res.status(201).json({
         status: 'success',
@@ -9,7 +12,7 @@ exports.createReview = catchAsync(async (req, res, next) => {
         }
     })
 })
-exports.getAllReviews = catchAsync(async (req,res,next)=>{
+exports.getAllReviews = catchAsync(async (req, res, next) => {
     const reviews = await Review.find().select('-__v')
     res.status(200).json({
         status: 'Success',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController')
 const authController = require('../controllers/authController')
+const reviewController = require('../controllers/reviewController')
 const router = express.Router()
 
 // router.param('id',tourController.checkID)
@@ -10,6 +11,15 @@ router.route('/top-5-cheap').get(tourController.topAliasTour, tourController.get
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(authController.protect, tourController.getAllTour).post(tourController.createTour)
-router.route('/:id').get(authController.protect, tourController.getTour).patch(tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
+router.route('/:id')
+    .get(authController.protect, tourController.getTour)
+    .patch(tourController.updateTour)
+    .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
 
+router.route('/:tourId/reviews')
+.post(
+    authController.protect, 
+    authController.restrictTo('user'), 
+    reviewController.getAllReviews
+);
 module.exports = router
