f01070149ea4f06990098707965a7603bc420e81
abfiker <new94abemelek@gmail.com>
2024-04-02 12:03:05 +0300

    adding a me endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
 router.patch('/updatePassword/:id', authController.updatePassword)
 
+router.get('/me',authController.protect,userController.getMe,userController.getUser)
 router.patch('/updateMe',authController.protect,userController.updateMe)
 router.delete('/deleteMe',authController.protect,userController.deleteMe)
 
