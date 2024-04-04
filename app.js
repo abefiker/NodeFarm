f9c0a36daddcd72a6a5eaa7d3cc0a4c54470bfcf
abfiker <new94abemelek@gmail.com>
2024-04-04 20:32:18 +0300

    setting up pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const app = express()
 const rateLimit = require('express-rate-limit')
@@ -12,6 +13,11 @@ const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
 //1) Global Middlewares
 //set secure http headers
 app.use(helmet())
@@ -37,7 +43,6 @@ app.use(hpp({
     ]
 }))
 //serving static files
-app.use(express.static(`${__dirname}/public`))
 //Test middleware
 app.use((req, res, next) => {
     console.log('Hello from the middleware 👋')
@@ -60,6 +65,9 @@ app.use((req, res, next) => {
 })
 
 //2) Routes
+app.get('/', (req, res) => {
+    res.status(200).render('base')
+})
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/reviews', reviewRouter)
